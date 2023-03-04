6e692941ee20bf7f8909985701d642bcb927ef42
ndelkamiller <123609285+ndelkamiller@users.noreply.github.com>
2023-03-04 12:55:02 -0600

    freaking map

--- docs/scripts/app.62d3f946.js
+++
@@ -1,2 +0,0 @@
-console.log('Hello bakers');
-//# sourceMappingURL=app.62d3f946.js.map
--- docs/scripts/nomodule/app.c04305a0.js
+++
@@ -1,5 +0,0 @@
-!(function () {
-  'use strict';
-  console.log('Hello bakers');
-})();
-//# sourceMappingURL=app.c04305a0.js.map
